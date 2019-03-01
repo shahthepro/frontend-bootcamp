1bbed7422312cc4f5601b97bc48bf310872e4db2
Shah <shahthepro@outlook.com>
2019-03-01 21:18:11 +0530

    Try step1-04

--- step1-04/demo/src/App.tsx
+++ step1-04/demo/src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Counter } from './components/Counter';
 
 export class App extends React.Component<any, any> {
   render() {
     let text = 'My App';
     return (
       <div className="App">
-        <h2>{text !== '' ? text : 'Default App Name'}</h2>
+        <Counter text="Cats"></Counter>
+        <Counter text="Dogs"></Counter>
+        <Counter text="Horses"></Counter>
       </div>
     );
   }
--- step1-04/demo/src/components/Counter.tsx
+++ step1-04/demo/src/components/Counter.tsx
@@ -1,7 +1,32 @@
 import React from 'react';
+import { Button } from './Button';
+import './Counter.css';
 
 export class Counter extends React.Component<any, any> {
+  boundedUpdateState: Function;
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      counter: 0
+    };
+
+    this.boundedUpdateState = this.updateState.bind(this);
+  }
+
   render() {
-    return <p>hello</p>;
+    return (
+      <div className="counter">
+        <div className="text">{this.state.counter} {this.props.text}</div>
+        <div className="button"><Button onClick={this.boundedUpdateState}>+1</Button></div>
+      </div>
+    );
+  }
+
+  updateState() {
+    this.setState({
+      counter: this.state.counter + 1
+    });
   }
 }
